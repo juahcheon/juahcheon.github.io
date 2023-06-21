7794b53256f57b6f49f764e5095dd5730c1cc251
juahcheon <132863519+juahcheon@users.noreply.github.com>
2023-06-21 17:22:56 +0900

    study_0621

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,8 +1,21 @@
 $(function(){
 
+    // 바탕화면 아이콘 테두리
+    $(".Wallpapers_icon").on("click", function(){
+        $(this).css("background-color", "#80b9ee72");
+        $(".Wallpapers_icon").not(this).css("background-color", "transparent");
+    });
 
 
+    $(".bg").on("click",function(){
+        $(".Wallpapers_icon").css("background-color", "transparent");
+    });
 
+    
+
+    
+
+    
 
 
 
